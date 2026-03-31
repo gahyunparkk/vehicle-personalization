f1c3d26cd6c512215cd49bf68dbbf4e85cd40299
KiwoongNam <nkw1725@gmail.com>
2026-03-31 19:20:54 +0900

    히스테리시스 적용

--- Ecu_Hvac_Hmi_TC375/BaseSw/Base_joystick.c
+++ Ecu_Hvac_Hmi_TC375/BaseSw/Base_joystick.c
@@ -12,29 +12,48 @@ void Joystick_init(void)
   IfxPort_setPinModeInput(JOYSW, IfxPort_InputMode_pullUp);
 }
 
-#define OKZONE_LOWER   50
-#define OKZONE_UPPER   4050
-#define DEADZONE_LOWER 1300
-#define DEADZONE_UPPER 2800
+#define OKZONE_LOWER_L 50
+#define OKZONE_LOWER_H 200
+#define OKZONE_UPPER_L 3850
+#define OKZONE_UPPER_H 4050
 
 joystick_dir_e Joystick_read(void)
 {
   uint16 x, y;
+  static joystick_dir_e dirx = JOY_NEUTRAL;
+  static joystick_dir_e diry = JOY_NEUTRAL;
+
   read_EVADC_Values(&x, &y);
-  if (y > DEADZONE_LOWER && y < DEADZONE_UPPER)
+  if (dirx == JOY_NEUTRAL)
   {
-    if (x < OKZONE_LOWER)
-      return JOY_LEFT;
-    else if (x > OKZONE_UPPER)
-      return JOY_RIGHT;
+    if (x < OKZONE_LOWER_L) dirx = JOY_LEFT;
+    else if (x > OKZONE_UPPER_H) dirx = JOY_RIGHT;
   }
-  else if (x > DEADZONE_LOWER && x < DEADZONE_UPPER)
+  else if (dirx == JOY_LEFT)
   {
-    if (y < OKZONE_LOWER)
-      return JOY_UP;
-    else if (y > OKZONE_UPPER)
-      return JOY_DOWN;
+    if (x > OKZONE_LOWER_H) dirx = JOY_NEUTRAL;
   }
+  else if (dirx == JOY_RIGHT)
+  {
+    if (x < OKZONE_UPPER_L) dirx = JOY_NEUTRAL;
+  }
+
+  if (diry == JOY_NEUTRAL)
+  {
+    if (y < OKZONE_LOWER_L) diry = JOY_UP;
+    else if (y > OKZONE_UPPER_H) diry = JOY_DOWN;
+  }
+  else if (diry == JOY_UP)
+  {
+    if (y > OKZONE_LOWER_H) diry = JOY_NEUTRAL;
+  }
+  else if (diry == JOY_DOWN)
+  {
+    if (y < OKZONE_UPPER_L) diry = JOY_NEUTRAL;
+  }
+
+  if (dirx == JOY_NEUTRAL) return diry;
+  if (diry == JOY_NEUTRAL) return dirx;
   return JOY_NEUTRAL;
 }
 
