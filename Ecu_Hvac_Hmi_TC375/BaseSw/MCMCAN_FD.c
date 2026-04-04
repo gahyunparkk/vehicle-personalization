6ce5da65f2c18215ffc4b453b601bfeaa83816bb
gudmin0526 <gudmin0526@gmail.com>
2026-04-04 18:41:37 +0900

    feat: 빌드 버전

--- Ecu_Hvac_Hmi_TC375/BaseSw/MCMCAN_FD.c
+++ Ecu_Hvac_Hmi_TC375/BaseSw/MCMCAN_FD.c
@@ -1,78 +1,151 @@
+// MCMCAN_FD.C
 #include "MCMCAN_FD.h"
-#include <string.h>
 
-mcmcanType g_mcmcan;
+McmcanType g_mcmcan;
 
-/* [추가] 통신 성공 횟수를 저장하는 카운터 (디버거 Variables 창에서 확인용) */
-volatile uint32 g_rxSuccessCount = 0;
+/* 핀 설정 구조체 */
+static const IfxCan_Can_Pins canPins =
+{
+    &TX_PIN, IfxPort_OutputMode_pushPull,
+    &RX_PIN, IfxPort_InputMode_pullUp,
+    IfxPort_PadDriver_cmosAutomotiveSpeed1
+};
+
+/* ⭐ 폴링 방식을 위해 인터럽트 핸들러는 최소화하거나 비워둡니다 ⭐ */
+// IFX_INTERRUPT(canIsrTxHandler, 0, ISR_PRIORITY_CAN_TX);
+void canIsrTxHandler(void)
+{
+    IfxCan_Node_clearInterruptFlag(g_mcmcan.canSrcNode.node, IfxCan_Interrupt_transmissionCompleted);
+}
+
+/* 수신 인터럽트는 사용하지 않으므로 핸들러 정의만 유지하거나 제거해도 됩니다. */
+
+#include "MCMCAN_FD.h"
+
+McmcanType g_mcmcan;
 
 void initMcmcan(void)
 {
-    /* 1. 트랜시버 깨우기 (P20.6 Low) */
+    /* 1. [시동] 트랜시버 전원 및 CAN 모듈 클럭 활성화 */
     IfxPort_setPinModeOutput(&MODULE_P20, 6, IfxPort_OutputMode_pushPull, IfxPort_OutputIdx_general);
-    IfxPort_setPinLow(&MODULE_P20, 6);
+    IfxPort_setPinLow(&MODULE_P20, 6); // 트랜시버 켜기
 
-    IfxCan_Can_Config canConfig;
-    IfxCan_Can_NodeConfig nodeConfig;
+    uint16 password = IfxScuWdt_getCpuWatchdogPassword();
+    IfxScuWdt_clearCpuEndinit(password);
+    MODULE_CAN0.CLC.B.DISR = 0;        // CAN 모듈에 전기 공급
+    IfxScuWdt_setCpuEndinit(password);
+    while(MODULE_CAN0.CLC.B.DISS);    // 완전히 깨어날 때까지 대기
 
-    /* 2. CAN 모듈 초기화 */
-    IfxCan_Can_initModuleConfig(&canConfig, &MODULE_CAN0);
-    IfxCan_Can_initModule(&g_mcmcan.canModule, &canConfig);
+    /* 2. [모듈] 모듈 레벨 초기화 (항상 노드 설정보다 먼저!) */
+    IfxCan_Can_initModuleConfig(&g_mcmcan.canConfig, &MODULE_CAN0);
+    IfxCan_Can_initModule(&g_mcmcan.canModule, &g_mcmcan.canConfig);
 
-    /* 3. CAN 노드 설정 */
-    IfxCan_Can_initNodeConfig(&nodeConfig, &g_mcmcan.canModule);
+    /* 3. [노드] 노드 기본 설정 초기화 */
+    IfxCan_Can_initNodeConfig(&g_mcmcan.canNodeConfig, &g_mcmcan.canModule);
 
-    nodeConfig.nodeId = IfxCan_NodeId_0;
-    nodeConfig.baudRate.baudrate = CAN_BAUDRATE;
-    nodeConfig.calculateBitTimingValues = TRUE;
+    g_mcmcan.canNodeConfig.pins = &canPins;
+    g_mcmcan.canNodeConfig.busLoopbackEnabled = FALSE;
+    g_mcmcan.canNodeConfig.nodeId = IfxCan_NodeId_0;
+    g_mcmcan.canNodeConfig.frame.type = IfxCan_FrameType_transmitAndReceive;
+    g_mcmcan.canNodeConfig.frame.mode = IfxCan_FrameMode_fdLongAndFast;
 
-    /* [핵심 수정] 275 송신기에 맞춰 FD 모드로 설정 */
-    /* 만약 275에서 Bit Rate Switch를 켰다면 IfxCan_FrameMode_fdLongAndFast를 사용하세요 */
-    nodeConfig.frame.mode = IfxCan_FrameMode_fdLongAndFast;
-    /* [핵심] 모든 ID를 수용하는 만능 필터 설정 */
-    nodeConfig.filterConfig.standardListSize = 0;
-    nodeConfig.filterConfig.standardFilterForNonMatchingFrames = IfxCan_NonMatchingFrame_acceptToRxFifo0;
+    /* TX interrupt */
+    g_mcmcan.canNodeConfig.interruptConfig.transmissionCompletedEnabled = TRUE;
+    g_mcmcan.canNodeConfig.interruptConfig.traco.priority = ISR_PRIORITY_CAN_TX;
+    g_mcmcan.canNodeConfig.interruptConfig.traco.interruptLine = IfxCan_InterruptLine_0;
+    g_mcmcan.canNodeConfig.interruptConfig.traco.typeOfService = IfxSrc_Tos_cpu0;
 
-    /* Message RAM 주소 배치 */
-    nodeConfig.rxConfig.rxMode = IfxCan_RxMode_sharedFifo0;
-    nodeConfig.rxConfig.rxFifo0Size = 32;
-    nodeConfig.rxConfig.rxFifo0DataFieldSize = IfxCan_DataFieldSize_8;
+    /* RX FIFO0 interrupt */
+    g_mcmcan.canNodeConfig.interruptConfig.rxFifo0NewMessageEnabled = TRUE;
+    g_mcmcan.canNodeConfig.interruptConfig.rxf0n.priority = ISR_PRIORITY_CAN_RX;
+    g_mcmcan.canNodeConfig.interruptConfig.rxf0n.interruptLine = IfxCan_InterruptLine_1;
+    g_mcmcan.canNodeConfig.interruptConfig.rxf0n.typeOfService = IfxSrc_Tos_cpu0;
 
-    nodeConfig.messageRAM.standardFilterListStartAddress = 0x0;
-    nodeConfig.messageRAM.rxFifo0StartAddress = 0x100;
+    /* [속도] 500k / 5M 설정 */
+    g_mcmcan.canNodeConfig.baudRate.baudrate = 500000;
+    g_mcmcan.canNodeConfig.fastBaudRate.baudrate = 5000000;
+    g_mcmcan.canNodeConfig.fastBaudRate.tranceiverDelayOffset = 12; // 지연 보상
+    g_mcmcan.canNodeConfig.calculateBitTimingValues = TRUE;
 
-    const IfxCan_Can_Pins pins = {
-        &IfxCan_TXD00_P20_8_OUT, IfxPort_OutputMode_pushPull,
-        &IfxCan_RXD00B_P20_7_IN, IfxPort_InputMode_pullUp,
-        IfxPort_PadDriver_cmosAutomotiveSpeed1
-    };
-    nodeConfig.pins = &pins;
+    /* ⭐ [수정] 메모리 RAM 사이즈 예약 (매우 중요!) ⭐ */
+    /* 이 값이 0이면 메모리가 할당되지 않아 수신 데이터가 계속 0으로 뜹니다. */
+    g_mcmcan.canNodeConfig.filterConfig.standardListSize = 1;      // 필터 1개 사용
+    g_mcmcan.canNodeConfig.rxConfig.rxFifo0Size = 16;             // FIFO 16칸 사용
 
-    /* 노드 초기화 실행 */
-    IfxCan_Can_initNode(&g_mcmcan.canNode, &nodeConfig);
+    g_mcmcan.canNodeConfig.messageRAM.baseAddress = (uint32)&MODULE_CAN0;
+    g_mcmcan.canNodeConfig.messageRAM.standardFilterListStartAddress = 0x100;
+    g_mcmcan.canNodeConfig.messageRAM.rxFifo0StartAddress = 0x200;
 
-    /* 버스 동기화 대기 */
-    while (IfxCan_Can_isNodeSynchronized(&g_mcmcan.canNode) != TRUE);
+    /* FD를 위해 주머니 크기를 64바이트로 통일 (8바이트 신호도 64 주머니에 담는 게 가장 안전함) */
+    g_mcmcan.canNodeConfig.messageRAM.txBuffersStartAddress = 0x600;
+    g_mcmcan.canNodeConfig.txConfig.txFifoQueueSize = 1;
+    g_mcmcan.canNodeConfig.txConfig.dedicatedTxBuffersNumber = 1;
+    g_mcmcan.canNodeConfig.txConfig.txBufferDataFieldSize = IfxCan_DataFieldSize_64; // ✅ FD는 반드시 64 지정
+    g_mcmcan.canNodeConfig.rxConfig.rxFifo0DataFieldSize  = IfxCan_DataFieldSize_64; // RX도 동일하게
+
+    /* ⭐ [안전장치] 필터에 안 맞아도 일단 FIFO 0로 받기 ⭐ */
+    /* Enum 이름 에러 방지를 위해 숫자 0 (Accept)으로 강제 캐스팅 */
+    g_mcmcan.canNodeConfig.filterConfig.standardFilterForNonMatchingFrames = (IfxCan_NonMatchingFrame)0;
+
+    /* RX FIFO0 settings */
+    g_mcmcan.canNodeConfig.rxConfig.rxMode = IfxCan_RxMode_fifo0;
+    g_mcmcan.canNodeConfig.rxConfig.rxFifo0Size = 16;
+    g_mcmcan.canNodeConfig.rxConfig.rxFifo0OperatingMode = IfxCan_RxFifoMode_blocking;
+
+    /* 노드 초기화 실행 (여기서 위 설정들이 하드웨어에 구워짐) */
+    IfxCan_Can_initNode(&g_mcmcan.canSrcNode, &g_mcmcan.canNodeConfig);
+
+    /* 5. [필터] 0x100 수신용 필터 설정 */
+    g_mcmcan.canFilter.number = 0;
+    g_mcmcan.canFilter.elementConfiguration = IfxCan_FilterElementConfiguration_storeInRxFifo0;
+    g_mcmcan.canFilter.type = IfxCan_FilterType_range;
+    g_mcmcan.canFilter.id1 = 0x100;
+    g_mcmcan.canFilter.id2 = 0x10F;
+
+    IfxCan_Can_setStandardFilter(&g_mcmcan.canSrcNode, &g_mcmcan.canFilter);
+
+    /* 동기화 대기 */
+    while (IfxCan_Can_isNodeSynchronized(&g_mcmcan.canSrcNode) != TRUE);
 }
 
+/* [송신] 기존 함수 유지 */
+void transmitCanMessage(uint32 txId, uint32 *pData)
+{
+    IfxCan_Can_initMessage(&g_mcmcan.txMsg);
+    g_mcmcan.txMsg.frameMode = IfxCan_FrameMode_fdLongAndFast;
+    g_mcmcan.txMsg.dataLengthCode = IfxCan_DataLengthCode_64;
+    g_mcmcan.txMsg.messageId = txId;
+
+    /* 64바이트 상자(txData)에 데이터 복사 */
+    for(int i = 0; i < 16; i++)
+    {
+        /* pData가 가리키는 곳에서 16개를 가져와 꽉 채웁니다 */
+        g_mcmcan.txData[i] = pData[i];
+    }
+    while( IfxCan_Status_notSentBusy ==
+           IfxCan_Can_sendMessage(&g_mcmcan.canSrcNode, &g_mcmcan.txMsg, &g_mcmcan.txData[0]) );
+}
+
+/* ⭐ [수신] TC275 스타일의 폴링 수신 함수 ⭐ */
 boolean receiveCanMessage(uint32 *rxData)
 {
-    /* [체크] N/A 방지를 위해 volatile 사용 */
-    volatile uint8 fillLevel = IfxCan_Can_getRxFifo0FillLevel(&g_mcmcan.canNode);
-
-    if (fillLevel > 0)
+    /* Rx FIFO 0에 메시지가 쌓여있는지 레벨 체크 */
+    if (IfxCan_Can_getRxFifo0FillLevel(&g_mcmcan.canSrcNode) > 0)
     {
         IfxCan_Message rxMsg;
         IfxCan_Can_initMessage(&rxMsg);
         rxMsg.readFromRxFifo0 = TRUE;
 
-        /* 데이터를 실제로 읽어옴 */
-        IfxCan_Can_readMessage(&g_mcmcan.canNode, &rxMsg, rxData);
+        /* FIFO에서 데이터를 긁어서 rxData 배열에 저장 */
+        IfxCan_Can_readMessage(&g_mcmcan.canSrcNode, &rxMsg, rxData);
 
-        /* [핵심 추가] 수신 성공 시 카운트 증가 */
-        g_rxSuccessCount++;
-
-        return TRUE;
+        /* g_mcmcan 구조체 내부 버퍼에도 동기화 */
+        for(int i = 0; i < 16; i++)
+        {
+            g_mcmcan.rxData[i] = rxData[i];
+        }
+        return TRUE; /* 수신 성공! */
     }
-    return FALSE;
+
+    return FALSE; /* 아직 들어온 데이터 없음 */
 }
--- Ecu_Hvac_Hmi_TC375/BaseSw/MCMCAN_FD.h
+++ Ecu_Hvac_Hmi_TC375/BaseSw/MCMCAN_FD.h
@@ -1,22 +1,40 @@
-
-#ifndef MCMCAN_FD_H_
-#define MCMCAN_FD_H_
+#ifndef MCMCAN_FD_H
+#define MCMCAN_FD_H
 
 #include "Ifx_Types.h"
 #include "IfxCan_Can.h"
+#include "IfxCan.h"
 #include "IfxPort.h"
+#include "IfxScuWdt.h"
+#include <string.h>
 
-#define CAN_BAUDRATE        500000
-#define RX_MESSAGE_ID       0x100
+/* 매크로 설정 */
+#define TX_PIN                  IfxCan_TXD00_P20_8_OUT
+#define RX_PIN                  IfxCan_RXD00B_P20_7_IN
+#define ISR_PRIORITY_CAN_TX     2
+#define ISR_PRIORITY_CAN_RX     1
+#define MAXIMUM_CAN_DATA_PAYLOAD 16      /* uint32 x 16 = 64 Bytes (FD) */
+#define INVALID_RX_DATA_VALUE   0xAAAAAAAA
 
+/* 구조체 정의 */
 typedef struct
 {
-    IfxCan_Can          canModule;
-    IfxCan_Can_Node     canNode;
-    /* MsgObj 핸들이 필요 없습니다! */
-} mcmcanType;
+    IfxCan_Can_Config       canConfig;
+    IfxCan_Can              canModule;
+    IfxCan_Can_Node         canSrcNode;         /* 메인 통신 노드 */
+    IfxCan_Can_NodeConfig   canNodeConfig;
+    IfxCan_Filter           canFilter;
+    IfxCan_Message          txMsg;
+    IfxCan_Message          rxMsg;
+    uint32                  txData[MAXIMUM_CAN_DATA_PAYLOAD];
+    uint32                  rxData[MAXIMUM_CAN_DATA_PAYLOAD];
+} McmcanType;
+
+/* 전역 변수 및 함수 선언 */
+extern McmcanType g_mcmcan;
 
 void initMcmcan(void);
+void transmitCanMessage(uint32 txId, uint32 *pData);
 boolean receiveCanMessage(uint32 *rxData);
 
 #endif
