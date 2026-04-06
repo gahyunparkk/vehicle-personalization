dd12a8438cf25026d6dc992b90579daa23b40315
KiwoongNam <nkw1725@gmail.com>
2026-04-06 19:48:52 +0900

    init seq도~

--- Ecu_Hvac_Hmi_TC375/AppSw/App_Manager_System.c
+++ Ecu_Hvac_Hmi_TC375/AppSw/App_Manager_System.c
@@ -43,6 +43,9 @@ static void App_Manager_System_CopyProfileTable(Shared_Profile_Table_t *dst,
 static void App_Manager_System_ReplaceProfileTable(Shared_Profile_Table_t *dst,
                                                    const Shared_Profile_Table_t *src);
 
+// init sequence
+static boolean App_PollProfileTableAtInit(uint32 timeout_ms);
+
 /*********************************************************************************************************************/
 /*------------------------------------------------Static Variables---------------------------------------------------*/
 /*********************************************************************************************************************/
@@ -89,6 +92,7 @@ void App_Manager_System_Init(void)
   g_app_manager_system_context.state_enter_time_ms = 0U;
   g_app_manager_system_context.active_profile_index = SHARED_PROFILE_INDEX_INVALID;
   g_app_manager_system_context.setup_profile_loaded = FALSE;
+  App_PollProfileTableAtInit(20000);
 }
 
 Shared_System_State_t App_Manager_System_GetState(void)
@@ -161,4 +165,83 @@ static void App_Manager_System_ReplaceProfileTable(Shared_Profile_Table_t *dst,
     dst->profile[index].seat_motor_angle = src->profile[index].seat_motor_angle;
     dst->profile[index].side_motor_angle = src->profile[index].side_motor_angle;
   }
+}
+
+/* -------------------------------------------------------------------------------------------------
+ * INIT CAN POLLING
+ * - 초기화 시점에 profile table을 일정 시간 동안 폴링으로 수신
+ * - 수신 성공 시 g_app.profileTable 갱신 후 TRUE 반환
+ * ------------------------------------------------------------------------------------------------- */
+static boolean App_PollProfileTableAtInit(uint32 timeout_ms)
+{
+  uint32 rx_data[MAXIMUM_CAN_DATA_PAYLOAD];
+  uint32 rx_id;
+  const uint8 *rx_bytes;
+  uint32 start_ms;
+
+  start_ms = App_GetNowMs();
+
+  while ((App_GetNowMs() - start_ms) < timeout_ms)
+  {
+    if (receiveCanMessage(rx_data) == FALSE)
+    {
+      continue;
+    }
+
+    rx_id = g_multican.rxMsg.id;
+    rx_bytes = (const uint8 *)rx_data;
+
+    switch (rx_id)
+    {
+    case SHARED_CAN_MSG_ID_SS_PROFILE_TABLE:
+    {
+      Shared_Profile_Table_t profile_table_msg;
+
+      (void)memset(&profile_table_msg, 0, sizeof(profile_table_msg));
+      (void)memcpy(&profile_table_msg,
+                   rx_bytes,
+                   sizeof(profile_table_msg));
+
+      (void)memcpy(&g_app_manager_system_context.profile_table,
+                   &profile_table_msg,
+                   sizeof(Shared_Profile_Table_t));
+
+      if (rx_id == SHARED_CAN_MSG_ID_SS_PROFILE_TABLE)
+      {
+        UART_Printf("[INIT][RX] SS_PROFILE_TABLE received\r\n");
+      }
+      else
+      {
+        UART_Printf("[INIT][RX] HH_PROFILE_TABLE received\r\n");
+      }
+
+      return TRUE;
+    }
+
+    case SHARED_CAN_MSG_ID_SS_STATE:
+    {
+      Shared_Can_State_t state_msg;
+
+      (void)memset(&state_msg, 0, sizeof(state_msg));
+      (void)memcpy(&state_msg,
+                   rx_bytes,
+                   sizeof(state_msg));
+
+      g_app_manager_system_context.current_state = state_msg.current_state;
+
+      UART_Printf("[INIT][RX] SS_STATE current_state=%u\r\n",
+                  state_msg.current_state);
+      break;
+    }
+
+    default:
+    {
+      /* init 단계에서는 profile table 외 메시지는 무시 */
+      break;
+    }
+    }
+  }
+
+  UART_Printf("[INIT][RX] PROFILE_TABLE timeout -> use default\r\n");
+  return FALSE;
 }
